2aba65f68ea9f205914e44014ea071ab983fa7f3
icedjello <revues.counter0x@icloud.com>
2024-03-17 19:10:21 +0000

    changed base to work with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "github-resume",
+  // base: "github-resume", // turned off because custom domain
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
